d0895b70b1d85fda2f69c578bef1f26141cfc537
João Ferreira <jpfn2407@gmail.com>
2020-12-08 16:19:42 +0000

    First commit

---
+++ src/controllers/Controller.java
@@ -0,0 +1,7 @@
+package controllers;
+
+public class Controller {
+    public Controller(){
+
+    }
+}
---
+++ src/views/CLI.java
@@ -0,0 +1,41 @@
+package views;
+import controllers.Controller;
+import java.util.*;
+
+public class CLI {
+    public static void main(String[] args){
+        Controller controller = new Controller();
+        Scanner scanner = new Scanner(System.in);
+        while(true){
+            String line = scanner.nextLine();
+            if(line == "") return;
+
+            String commands[] = line.split(" ");
+
+            switch(commands[0]) {
+                case "RF":
+                    break;
+                case "RC":
+                    break;
+                case "RI":
+                    break;
+                case "RL":
+                    break;
+                case "RD":
+                    break;
+                case "RE":
+                    break;
+                case "CC":
+                    break;
+                case "CI":
+                    break;
+                case "CE":
+                    break;
+                case "CF":
+                    break;
+                default:
+                    System.out.println("Instrução Inválida.");
+            }
+        }
+    }
+}
